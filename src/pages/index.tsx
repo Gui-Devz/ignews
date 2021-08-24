71e679686e74942967df7a40da7a74fcfe26ff64
Guilherme <guilherme_devz@hotmail.com>
2021-08-24 18:33:20 -0300

    UPDATE: correcting the name of the function in line 43

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1JCTbFHSH1mvgvtPdTnYr2Yg");
 
   const product = {
