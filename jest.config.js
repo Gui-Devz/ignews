172024f1f62ed56116527dbbe329612f01253c15
Guilherme <guilherme_devz@hotmail.com>
2021-08-24 18:27:32 -0300

    UPDATE: configuring the JEST for accepting css and sass files and to generate a coverage report

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,13 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy",
+  },
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx",
+  ],
+  coverageReporters: ["lcov", "json"],
 };
